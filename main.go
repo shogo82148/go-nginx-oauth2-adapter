28e417899fb36009adfac8fcc17c9129b7c5c8ea
Ichinose Shogo <shogo82148@gmail.com>
2016-02-29 01:43:54 +0900

    yaml config file

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package adapter
 
 import (
+	"flag"
+	"fmt"
 	"math/rand"
 	"time"
 )
 
 func Main() {
+	var configFile string
+	flag.StringVar(&configFile, "c", "", "configuration file")
+	flag.StringVar(&configFile, "config", "", "configuration file")
+	flag.Parse()
+
 	rand.Seed(time.Now().UnixNano())
 
 	c := NewConfig()
 	if err := c.LoadEnv(); err != nil {
 		panic(err)
 	}
+	if configFile != "" {
+		if err := c.LoadYaml(configFile); err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println(c)
 	s, err := NewServer(*c)
 	if err != nil {
 		panic(err)
