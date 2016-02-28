00a5119c819325522533d588745fe1e51929d042
Ichinose Shogo <shogo82148@gmail.com>
2016-02-28 22:48:45 +0900

    save urls into the session

--- adapter.go
+++ adapter.go
@@ -5,20 +5,26 @@ import (
 	"net"
 	"net/http"
 
+	"github.com/gorilla/sessions"
+
 	"golang.org/x/oauth2"
 )
 
+const DefaultSessionName = "go-nginx-oauth2-session"
+
 type Config struct {
-	Host      string                            `yaml:"host", json:"host"`
-	Port      string                            `yaml:"port", json:"port"`
-	Secret    string                            `yaml:"secret", json:"scret"`
-	Providers map[string]map[string]interface{} `yaml:"providers", json:"providers"`
+	Host        string                            `yaml:"host", json:"host"`
+	Port        string                            `yaml:"port", json:"port"`
+	Secret      string                            `yaml:"secret", json:"scret"`
+	SessionName string                            `yaml:"session_name", json:"session_name"`
+	Providers   map[string]map[string]interface{} `yaml:"providers", json:"providers"`
 }
 
 type Server struct {
 	Config          Config
 	DefaultPrivider string
 	ProviderConfigs map[string]ProviderConfig
+	SessionStore    sessions.Store
 }
 
 func Main() {
@@ -55,6 +61,12 @@ func NewServer(config Config) (*Server, error) {
 		s.DefaultPrivider = name
 	}
 
+	s.SessionStore = sessions.NewCookieStore([]byte(config.Secret))
+
+	if s.Config.SessionName == "" {
+		s.Config.SessionName = DefaultSessionName
+	}
+
 	return s, nil
 }
 
@@ -87,15 +99,53 @@ func (s *Server) HandlerTest(w http.ResponseWriter, r *http.Request) {
 
 // HandlerInitiate redirects to authorization page.
 func (s *Server) HandlerInitiate(w http.ResponseWriter, r *http.Request) {
+	session, err := s.SessionStore.Get(r, s.Config.SessionName)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+
 	conf := s.ProviderConfigs[s.DefaultPrivider].Config()
-	conf.RedirectURL = r.Header.Get("x-ngx-omniauth-initiate-callback")
-	state := r.Header.Get("x-ngx-omniauth-initiate-back-to")
-	http.Redirect(w, r, conf.AuthCodeURL(state), http.StatusFound)
+	callback := r.Header.Get("x-ngx-omniauth-initiate-callback")
+	next := r.Header.Get("x-ngx-omniauth-initiate-back-to")
+
+	conf.RedirectURL = callback
+	session.Values["callback"] = callback
+	session.Values["next"] = next
+	session.Save(r, w)
+
+	// TODO: state is recommended
+	http.Redirect(w, r, conf.AuthCodeURL(""), http.StatusFound)
 }
 
 // HandlerCallback validates the user infomation, set to cookie
 func (s *Server) HandlerCallback(w http.ResponseWriter, r *http.Request) {
+	session, err := s.SessionStore.Get(r, s.Config.SessionName)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+
 	conf := s.ProviderConfigs[s.DefaultPrivider].Config()
+
+	var val interface{}
+	var ok bool
+
+	var callback string
+	val = session.Values["callback"]
+	if callback, ok = val.(string); !ok {
+		fmt.Println("callback is not set")
+		http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+		return
+	}
+	conf.RedirectURL = callback
+
+	var next string
+	val = session.Values["next"]
+	if next, ok = val.(string); !ok {
+		fmt.Println("next is not set")
+		http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+		return
+	}
+
 	code := r.URL.Query().Get("code")
 	t, err := conf.Exchange(oauth2.NoContext, code)
 	if err != nil {
@@ -105,5 +155,7 @@ func (s *Server) HandlerCallback(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_ = t
-	//http.Redirect(w, r, "/", http.StatusFound)
+
+	session.Save(r, w)
+	http.Redirect(w, r, next, http.StatusFound)
 }
