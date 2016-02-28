48749d448fcffb858bd1adfe39f33f9f0bb9e5c7
Ichinose Shogo <shogo82148@gmail.com>
2016-02-28 22:13:26 +0900

    support Object-orientation-like interface

--- adapter.go
+++ adapter.go
@@ -1,41 +1,105 @@
 package adapter
 
 import (
+	"fmt"
+	"net"
 	"net/http"
 
 	"golang.org/x/oauth2"
 )
 
-var conf oauth2.Config
+type Config struct {
+	Host      string                            `yaml:"host", json:"host"`
+	Port      string                            `yaml:"port", json:"port"`
+	Secret    string                            `yaml:"secret", json:"scret"`
+	Providers map[string]map[string]interface{} `yaml:"providers", json:"providers"`
+}
+
+type Server struct {
+	Config          Config
+	DefaultPrivider string
+	ProviderConfigs map[string]ProviderConfig
+}
 
 func Main() {
-	provider := providers["google_oauth2"]
-	providerConfig, _ := provider.ParseConfig(map[string]interface{}{})
-	conf = providerConfig.Config()
+	s, err := NewServer(Config{})
+	if err != nil {
+		panic(err)
+	}
 
-	http.HandleFunc("/test", HandlerTest)
-	http.HandleFunc("/initiate", HandlerInitiate)
-	http.HandleFunc("/callback", HandlerCallback)
-	http.ListenAndServe(":8081", nil)
+	s.ListenAndServe()
+}
+
+func NewServer(config Config) (*Server, error) {
+	s := &Server{
+		Config:          config,
+		ProviderConfigs: map[string]ProviderConfig{},
+	}
+
+	for name, provider := range providers {
+		var conf map[string]interface{}
+		var ok bool
+		if config.Providers != nil {
+			conf, ok = config.Providers[name]
+			if !ok {
+				conf = map[string]interface{}{}
+			}
+		} else {
+			conf = map[string]interface{}{}
+		}
+		providerConfig, err := provider.ParseConfig(conf)
+		if err != nil {
+			return nil, err
+		}
+		s.ProviderConfigs[name] = providerConfig
+		s.DefaultPrivider = name
+	}
+
+	return s, nil
+}
+
+func (s *Server) ListenAndServe() error {
+	host := s.Config.Host
+	port := s.Config.Port
+	if port == "" {
+		port = "18080"
+	}
+	return http.ListenAndServe(net.JoinHostPort(host, port), s)
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/test":
+		s.HandlerTest(w, r)
+	case "/initiate":
+		s.HandlerInitiate(w, r)
+	case "/callback":
+		s.HandlerCallback(w, r)
+	default:
+		http.NotFound(w, r)
+	}
 }
 
 // HandlerTest validates the session.
-func HandlerTest(w http.ResponseWriter, r *http.Request) {
+func (s *Server) HandlerTest(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 }
 
 // HandlerInitiate redirects to authorization page.
-func HandlerInitiate(w http.ResponseWriter, r *http.Request) {
+func (s *Server) HandlerInitiate(w http.ResponseWriter, r *http.Request) {
+	conf := s.ProviderConfigs[s.DefaultPrivider].Config()
 	conf.RedirectURL = r.Header.Get("x-ngx-omniauth-initiate-callback")
 	state := r.Header.Get("x-ngx-omniauth-initiate-back-to")
 	http.Redirect(w, r, conf.AuthCodeURL(state), http.StatusFound)
 }
 
 // HandlerCallback validates the user infomation, set to cookie
-func HandlerCallback(w http.ResponseWriter, r *http.Request) {
+func (s *Server) HandlerCallback(w http.ResponseWriter, r *http.Request) {
+	conf := s.ProviderConfigs[s.DefaultPrivider].Config()
 	code := r.URL.Query().Get("code")
 	t, err := conf.Exchange(oauth2.NoContext, code)
 	if err != nil {
+		fmt.Println(err)
 		http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
 		return
 	}
