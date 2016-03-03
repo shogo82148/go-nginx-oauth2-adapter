20e6568df0bf334a85a1dc88fc5a5c5e52b8418b
Ichinose Shogo <shogo82148@gmail.com>
2016-03-04 01:29:01 +0900

    fix typo

--- examples_nginx_test.go
+++ examples_nginx_test.go
@@ -55,10 +55,10 @@ func TestNginx(t *testing.T) {
 
 	go http.ListenAndServe(":18082", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if got, expected := r.Header.Get("x-ngx-omniauth-provider"), "development"; got != expected {
-			t.Errorf("want %s, got %s", expected, git)
+			t.Errorf("want %s, got %s", expected, got)
 		}
 		if got, expected := r.Header.Get("x-ngx-omniauth-user"), "developer"; got != expected {
-			t.Errorf("want %s, got %s", expected, git)
+			t.Errorf("want %s, got %s", expected, got)
 		}
 		if r.Header.Get("x-ngx-omniauth-info") == "" {
 			t.Errorf("want x-ngx-omniauth-info is set, but empty")
