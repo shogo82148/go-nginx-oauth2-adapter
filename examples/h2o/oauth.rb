7cc2f8a57bc8463ba44fef3a6b9bba51f13aabcd
Ichinose Shogo <shogo82148@gmail.com>
2017-06-27 11:05:26 +0900

    improve h2o handler

--- examples/h2o/oauth.rb
+++ examples/h2o/oauth.rb
@@ -1,38 +1,50 @@
-lambda do |env|
-  auth_server = "http://127.0.0.1:18081"
-  auth_callback = "/_auth/callback"
+class OAuth2Adapter
 
-  if env["PATH_INFO"] == auth_callback
-    req = http_request(
-      "#{auth_server}/callback?#{env["QUERY_STRING"]}",
-      headers: {
-        "COOKIE" => env["HTTP_COOKIE"],
-      }
-    )
-    return req.join
+  def initialize(config={})
+    config = {
+      :auth_server = "http://127.0.0.1:18081",
+      :auth_callback = "/_auth/callback",
+    }.merge(config)
+    @auth_server = config[:auth_server]
+    @auth_callback = config[:auth_callback]
+    raise "auth_server must not be nil" if @auth_server.nil?
+    raise "auth_callback must not be nil" if @auth_callback.nil?
   end
 
-  req = http_request(
-    "#{auth_server}/test",
-    headers: {
-      "COOKIE"                      => env["HTTP_COOKIE"],
-      "X-NGX-OMNIAUTH-ORIGINAL-URI" => env["rack.url_scheme"] + "://" + env["HTTP_HOST"] + env["PATH_INFO"],
-    })
-  status, testheaders, body = req.join
+  def call(env)
+    if env["PATH_INFO"] == @auth_callback
+      req = http_request(
+        "#{@auth_server}/callback?#{env["QUERY_STRING"]}",
+        headers: {
+          "COOKIE" => env["HTTP_COOKIE"],
+        }
+      )
+      return req.join
+    end
 
-  if status == 401
-    # not login, redirect to authorization page
     req = http_request(
-      "#{auth_server}/initiate",
+      "#{@auth_server}/test",
       headers: {
-        "COOKIE"                           => env["HTTP_COOKIE"],
-        "X-NGX-OMNIAUTH-INITIATE-BACK-TO"  => env["rack.url_scheme"] + "://" + env["HTTP_HOST"] + env["PATH_INFO"],
-        "X-NGX-OMNIAUTH-INITIATE-CALLBACK" => env["rack.url_scheme"] + "://" + env["HTTP_HOST"] + auth_callback,
+        "COOKIE"                      => env["HTTP_COOKIE"],
+        "X-NGX-OMNIAUTH-ORIGINAL-URI" => env["rack.url_scheme"] + "://" + env["HTTP_HOST"] + env["PATH_INFO"],
       })
-    return req.join
-  elsif status < 200 || 300 <= status
-    return [403, {'content-type' => 'text/plain'}, ["Forbidden\n"]]
+    status, testheaders, body = req.join
+
+    if status == 401
+      # not login, redirect to authorization page
+      req = http_request(
+        "#{@auth_server}/initiate",
+        headers: {
+          "COOKIE"                           => env["HTTP_COOKIE"],
+          "X-NGX-OMNIAUTH-INITIATE-BACK-TO"  => env["rack.url_scheme"] + "://" + env["HTTP_HOST"] + env["PATH_INFO"],
+          "X-NGX-OMNIAUTH-INITIATE-CALLBACK" => env["rack.url_scheme"] + "://" + env["HTTP_HOST"] + @auth_callback,
+        })
+      return req.join
+    elsif status < 200 || 300 <= status
+      return [403, {'content-type' => 'text/plain'}, ["Forbidden\n"]]
+    end
+
+    return [399, {}, []]
   end
 
-  return [399, {}, []]
 end
