c1ab9ca965e7505be61fcefc10edf4ebb4b11697
Ichinose Shogo <shogo82148@gmail.com>
2020-04-11 16:58:18 +0900

    fix typo

--- adapter_test.go
+++ adapter_test.go
@@ -8,7 +8,7 @@ func TestNewServer(t *testing.T) {
 	String := func(s string) *string {
 		return &s
 	}
-	dummyProvidor := map[string]map[string]interface{}{"development": {}}
+	dummyProvider := map[string]map[string]interface{}{"development": {}}
 
 	testcases := []struct {
 		err bool
@@ -24,13 +24,13 @@ func TestNewServer(t *testing.T) {
 		{
 			err: false, // just warn in running.
 			c: Config{
-				Providers: dummyProvidor,
+				Providers: dummyProvider,
 			},
 		},
 		{
 			err: true, // fail in testing the config.
 			c: Config{
-				Providers:  dummyProvidor,
+				Providers:  dummyProvider,
 				ConfigTest: true,
 			},
 		},
@@ -39,28 +39,28 @@ func TestNewServer(t *testing.T) {
 		{
 			err: false, // the secret is 32 bytes, and it's valid.
 			c: Config{
-				Providers: dummyProvidor,
+				Providers: dummyProvider,
 				Secrets:   []*string{String("dummy-session-authentication-key")},
 			},
 		},
 		{
 			err: true, // the secret is 33 bytes, and it's invalid.
 			c: Config{
-				Providers: dummyProvidor,
+				Providers: dummyProvider,
 				Secrets:   []*string{String("dummy-session-authentication-key+")},
 			},
 		},
 		{
 			err: false, // valid hex string
 			c: Config{
-				Providers: dummyProvidor,
+				Providers: dummyProvider,
 				Secrets:   []*string{String("8e26ea01bd8805788bcb4660c7c15692e4771b5d6a22635eede025ca544ad4a00bcd17295f1ca8a5d573899fc7a641a25f488c9a5e839368cd79c2ffe1028031")},
 			},
 		},
 		{
 			err: true, // invalid hex string
 			c: Config{
-				Providers: dummyProvidor,
+				Providers: dummyProvider,
 				Secrets:   []*string{String("INVALID-HEX-05788bcb4660c7c15692e4771b5d6a22635eede025ca544ad4a00bcd17295f1ca8a5d573899fc7a641a25f488c9a5e839368cd79c2ffe1028031")},
 			},
 		},
@@ -69,28 +69,28 @@ func TestNewServer(t *testing.T) {
 		{
 			err: false, // the secret is 32 bytes, and it's valid.
 			c: Config{
-				Providers: dummyProvidor,
+				Providers: dummyProvider,
 				Secrets:   []*string{nil, String("**dummy-session-encryption-key**")},
 			},
 		},
 		{
 			err: true, // the secret is 33 bytes, and it's invalid.
 			c: Config{
-				Providers: dummyProvidor,
+				Providers: dummyProvider,
 				Secrets:   []*string{nil, String("dummy-session-encryption-key")},
 			},
 		},
 		{
 			err: false, // valid hex string
 			c: Config{
-				Providers: dummyProvidor,
+				Providers: dummyProvider,
 				Secrets:   []*string{nil, String("5c9ea31b400099a521f934f8a4c2c88758ca59e0a34479775aea86404921658e")},
 			},
 		},
 		{
 			err: true, // invalid hex string
 			c: Config{
-				Providers: dummyProvidor,
+				Providers: dummyProvider,
 				Secrets:   []*string{nil, String("INVALID-HEX-5c9ea31b400099a521f934f8a4c2c88758ca59e0a34479775aea")},
 			},
 		},
@@ -99,7 +99,7 @@ func TestNewServer(t *testing.T) {
 		{
 			err: true,
 			c: Config{
-				Providers:          dummyProvidor,
+				Providers:          dummyProvider,
 				AppRefreshInterval: "hoge",
 			},
 		},
