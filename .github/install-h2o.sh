05fcb9fcab7a21268216b6875aa3c1e93d7180f8
Ichinose Shogo <shogo82148@gmail.com>
2020-07-13 08:14:16 +0900

    install h2o

---
+++ .github/install-h2o.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eux
+
+H2O_VERSION=2.2.5
+
+if [[ ! -d "$HOME/h2o-$H2O_VERSION" ]]; then
+    cd ~/
+    curl -OL "https://github.com/h2o/h2o/archive/v$H2O_VERSION.tar.gz"
+    tar xzf "v$H2O_VERSION.tar.gz"
+fi
+
+cd "$HOME/h2o-$H2O_VERSION"
+cmake -DWITH_BUNDLED_SSL=on -DWITH_MRUBY=on .
+make
+sudo make install
